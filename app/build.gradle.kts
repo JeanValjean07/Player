7ead65ec5c32ad8b81c263958f830ce9948cdb68
SuMing <jeanval1749@gmail.com>
2025-10-06 13:26:20 +0800

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "2.4.0"
+        versionName = "2.6.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
