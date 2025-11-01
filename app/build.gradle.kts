c16628f8ef22a576a548da1a17941413ef9b5010
SuMing <jeanval1749@gmail.com>
2025-11-01 09:33:30 +0800

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "3.5.0"
+        versionName = "3.5.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
